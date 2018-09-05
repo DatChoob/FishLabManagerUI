ba45ea5e5c8c6df50253ed164c1aa06ef53428fc
PotatoCoder3000 <31225100+PotatoCoder3000@users.noreply.github.com>
2018-09-04 19:21:20 -0700

    Admin Room tab with editable table

--- src/app/logged-in/admin/admin-rooms/admin-rooms.component.ts
+++ src/app/logged-in/admin/admin-rooms/admin-rooms.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { TableDataSource, TableElement } from 'angular4-material-table';
+import { DialogService } from '../../../shared/dialogs.service';
+import { Observable } from 'rxjs';
+import { AdminRoomValidatorService } from './validator.service';
 
 @Component({
   selector: 'app-admin-rooms',
@@ -6,10 +10,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-rooms.component.css']
 })
 export class AdminRoomsComponent implements OnInit {
+  dataSource:TableDataSource<Room>;
+  
+  displayedColumns = [ 'building', 'roomNumber','actionsColumn']; 
 
-  constructor() { }
+  constructor(private adminRoomValidatorService: AdminRoomValidatorService, private dialogService: DialogService) { }
+
+  @Input() taskList: Room[] = [
+    { position: 0, building: 'Mark', roomNumber: 15 },
+     { position: 1, building: 'Brad', roomNumber: 50 },
+  ];
 
   ngOnInit() {
+    this.dataSource  = new TableDataSource<Room>(this.taskList, Room,this.adminRoomValidatorService);
   }
 
+  confirmSave(row: TableElement<Room>) {
+    console.log(row);
+    if (row.id == -1) {
+      row.currentData.position = this.taskList.length;
+
+      // we are creating a new row
+    } else {
+      //we are editing a row
+    }
+    row.confirmEditCreate();
+  }
+
+  
+  cancelOrDelete(row: TableElement<any>) {
+    console.log(row);
+    if (!row.editing) {
+      //means row was in not edit mode and we are deleting entry
+      //delete row from database
+      this.openDialog().subscribe(result => {
+        console.log('The dialog was closed ' + result);
+      });
+    }
+    row.cancelOrDelete();
+  }
+
+  openDialog(): Observable<boolean> {
+    return this.dialogService
+      .confirm('Confirm Dialog', 'Are you sure you want to do this?')
+
+  }
+  
+}
+
+class Room {
+  position: number;
+  building: string;
+  roomNumber: number;
 }
