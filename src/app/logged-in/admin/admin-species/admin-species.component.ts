f6e3063a7238bee1d4e587230b40baf0211b798e
Kashmita <reddyka22@gmail.com>
2018-09-11 20:07:48 -0700

    "Added Species Service"

--- src/app/logged-in/admin/admin-species/admin-species.component.ts
+++ src/app/logged-in/admin/admin-species/admin-species.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit, Input } from '@angular/core';
 import { TableDataSource, TableElement } from 'angular4-material-table';
 import { Species } from '../../../shared/models/species';
 
+import { Observable } from 'rxjs';
+import { cloneDeep } from 'lodash';
+import { DialogService } from '../../../shared/dialogs.service';
+import { SpeciesService } from '../../../shared/api-services/species.service';
+
+
+
 @Component({
   selector: 'app-admin-species',
   templateUrl: './admin-species.component.html',
@@ -9,45 +16,51 @@ import { Species } from '../../../shared/models/species';
 })
 export class AdminSpeciesComponent implements OnInit {
 
-  constructor() { }
   displayedColumns = ['originalName', 'currentName', 'commonName', 'actionsColumn'];
-
-  @Input() speciesList: Species[] = [
-    { id: 0, originalName: 'Mark', commonName: 'MarkC', currentName: 'Marc' },
-    { id: 1, originalName: 'Brad', commonName: 'BradC', currentName: 'Bard' },
-  ];
-
   dataSource: TableDataSource<Species>;
-  ngOnInit() {
-    this.dataSource = new TableDataSource<Species>(this.speciesList, Species);
 
-    this.dataSource.datasourceSubject.subscribe(taskList => {
-      console.log(taskList);
+  constructor(private speciesService: SpeciesService, private dialogService : DialogService) { }
 
+ngOnInit(){
+ this.speciesService.speciesList.subscribe(data => {
+   let clone: Species[] = cloneDeep(data);
+   this.dataSource = new TableDataSource(clone);
+ })
+}
+
+  
+confirmSave(row: TableElement<Species>){
+ if(row.validator.valid && !!row.currentData.id){
+   this.speciesService.createOrUpdate(row.currentData).subscribe(
+     allSpecies => {
+       row.confirmEditCreate();
+     }, err => console.log(err));
+ }
+}
+  
+cancelOrDelete(row: TableElement<Species>){
+  if(!row.editing){
+    this.openDialog().subscribe(userConfirmed =>{
+      if(userConfirmed){
+        this.speciesService.deleteSpecies(row.currentData).subscribe(resonse => {});
+      }
     });
-
-  }
-
-
-  confirmSave(row: TableElement<any>) {
-    console.log(this.speciesList);
-    console.log(row);
-    if (row.id == -1) {
-      row.currentData.index=this.speciesList.length;
-      
-      // we are creating a new row
-    } else {
-      //we are editing a row
-    }
-    row.confirmEditCreate();
-  }
-
-  cancelOrDelete(row: TableElement<any>) {
-    console.log(row);
-    if (!row.editing) {
-      //means row was in not edit mode and we are deleting entry
-      //delete row from database
-    }
+  }else{
     row.cancelOrDelete();
   }
 }
+
+openDialog(): Observable<boolean>{
+  return this.dialogService.confirm('Confirm Dialog', 'Are you sure you want to do this?')
+}
+
+}
+
+
+
+
+
+
+
+
+
