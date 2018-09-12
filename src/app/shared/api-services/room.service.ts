a0a6c6173146dfa19ab23ba7c7a5962ebfc4afaa
Josh Singh <jashmit04@yahoo.com>
2018-09-11 17:13:34 -0700

    Removing private method

--- src/app/shared/api-services/room.service.ts
+++ src/app/shared/api-services/room.service.ts
@@ -1,15 +1,15 @@
-import { Room } from './../models/room';
 import { Injectable } from '@angular/core';
 import { Observable, of, BehaviorSubject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Room } from '../models/room';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TaskService {
+export class tankResponseService {
 
-  room: BehaviorSubject<Room[]> = new BehaviorSubject<Room[]>(
+  rooms: BehaviorSubject<Room[]> = new BehaviorSubject<Room[]>(
     [
       { id: 1, building: 'Humboldt', roomNumber: 121},
       { id: 2, building: 'Humboldt', roomNumber: 123}
@@ -23,48 +23,35 @@ export class TaskService {
    * the api will respond back with the update/new object. 
    * if the data coming in doesnt have an id, then we know this will be a new object. 
    * @param data data to add to database
-   * @param isGlobalTask is this data for a global task?
+   * @param isGlobaltankResponse is this data for a global tankResponse?
    */
-  createOrUpdate(roomToCreateOrUpdate: Room, isGlobalTask: boolean): Observable<Room[]> {
-    return this.createOrUpdateRoom(roomToCreateOrUpdate);
-  }
-
-  private createOrUpdateRoom(roomToCreateOrUpdate: Room) {
-    let originalData = this.room.getValue();
+  createOrUpdate(roomToCreateOrUpdate: Room, isGlobaltankResponse: boolean): Observable<Room[]> {
+    let originalData = this.rooms.getValue();
     //TODO: remove of() with http request
     return of(roomToCreateOrUpdate).pipe(
-      map(task => {
+      map(tankResponse => {
         if (roomToCreateOrUpdate.id == null) {
-          //This is a create. In real api call, the task will already have the id. we assigning one for now
-          task.id = 21;
-          originalData.push(task);
+          //This is a create. In real api call, the tankResponse will already have the id. we assigning one for now
+          tankResponse.id = 21;
+          originalData.push(tankResponse);
         } else {
           //this is an update. find the index by id and replace the item at the index with our updated one
-          let indexToUpdate = this.room.value.findIndex(roomTask => roomTask.id == roomToCreateOrUpdate.id);
+          let indexToUpdate = this.rooms.value.findIndex(roomtankResponse => roomtankResponse.id == roomToCreateOrUpdate.id);
           originalData[indexToUpdate] = roomToCreateOrUpdate;
         }
 
-        this.room.next(originalData);
-        return this.room.value;
+        this.rooms.next(originalData);
+        return this.rooms.value;
       })
     );
   }
 
-
-  deleteTask(roomToDelete: Room) {
-    return this.deleteRoom(roomToDelete);
+  deletetankResponse(roomToDelete: Room) {
+      //TODO replace this with a real http request to delete by id.
+      let indexToDelete = this.rooms.value.findIndex(roomtankResponse => roomtankResponse.id == roomToDelete.id);
+      this.rooms.value.splice(indexToDelete, 1);
+      this.rooms.next(this.rooms.value);
+      return of(true);
 
   }
-
-  private deleteRoom(roomToDelete) {
-    //TODO replace this with a real http request to delete by id.
-    let indexToDelete = this.room.value.findIndex(roomTask => roomTask.id == roomToDelete.id);
-    this.room.value.splice(indexToDelete, 1);
-    this.room.next(this.room.value);
-    return of(true);
-  }
-
-
-
-
 }
